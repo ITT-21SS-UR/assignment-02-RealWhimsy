82ec72ad39c6f036b726c06c1ae4d4a6e4a937b1
RealWhimsy <lukas.jack@web.de>
2021-04-24 08:34:11 -0400

    Keyboard and button inputs are now logged separately

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 
 import sys
 from PyQt5 import uic, Qt
+from enum import Enum
 
 CLEAR = "Clear"
 BACKSPACE = "Backspace"
@@ -11,15 +12,30 @@ app = Qt.QApplication(sys.argv)
 win = uic.loadUi("calculator.ui")
 
 
+class InputType(Enum):
+    """
+    enum used to differentiate between the two input methods
+    """
+    BUTTON = 1
+    KEYBOARD = 2
+
+
 def logging_decorator(func):
-    def wrapper(arg):
-        func(arg)
-        print("Input: " + str(arg))
+    def wrapper(arg, input_type):
+        func(arg, input_type)
+
+        # check how the input was entered and print accordingly
+        if input_type == InputType.KEYBOARD:
+            print("Keyboard Input: " + str(arg))
+        elif input_type == InputType.BUTTON:
+            print("Button clicked: " + str(arg))
+        else:
+            print("Input Type did not match any valid value!")
     return wrapper
 
 
 @logging_decorator
-def evaluate(arg=EVALUATE):
+def evaluate(arg, input_type):
     # replace ',' with '.'
     expression = win.eval_text.toPlainText().replace(",", ".")
 
@@ -30,52 +46,59 @@ def evaluate(arg=EVALUATE):
 
 
 @logging_decorator
-def backspace(arg=BACKSPACE):
+def backspace(arg, input_type):
     win.eval_text.setText(win.eval_text.toPlainText()[:-1])
 
 
 @logging_decorator
-def clear(arg=CLEAR):
+def clear(arg, input_type):
     win.eval_text.setText("")
 
 
 @logging_decorator
-def add_input_to_text(user_input):
+def add_input_to_text(user_input, input_type):
     win.eval_text.setText(win.eval_text.toPlainText() + str(user_input))
 
 
 def set_click_listeners():
-    win.button_0.clicked.connect(lambda: add_input_to_text(0))
-    win.button_1.clicked.connect(lambda: add_input_to_text(1))
-    win.button_2.clicked.connect(lambda: add_input_to_text(2))
-    win.button_3.clicked.connect(lambda: add_input_to_text(3))
-    win.button_4.clicked.connect(lambda: add_input_to_text(4))
-    win.button_5.clicked.connect(lambda: add_input_to_text(5))
-    win.button_6.clicked.connect(lambda: add_input_to_text(6))
-    win.button_7.clicked.connect(lambda: add_input_to_text(7))
-    win.button_8.clicked.connect(lambda: add_input_to_text(8))
-    win.button_9.clicked.connect(lambda: add_input_to_text(9))
-    win.button_decimal.clicked.connect(lambda: add_input_to_text("."))
+    """
+    sets the click listeners for all buttons
+    """
+    win.button_0.clicked.connect(lambda: add_input_to_text(0, InputType.BUTTON))
+    win.button_1.clicked.connect(lambda: add_input_to_text(1, InputType.BUTTON))
+    win.button_2.clicked.connect(lambda: add_input_to_text(2, InputType.BUTTON))
+    win.button_3.clicked.connect(lambda: add_input_to_text(3, InputType.BUTTON))
+    win.button_4.clicked.connect(lambda: add_input_to_text(4, InputType.BUTTON))
+    win.button_5.clicked.connect(lambda: add_input_to_text(5, InputType.BUTTON))
+    win.button_6.clicked.connect(lambda: add_input_to_text(6, InputType.BUTTON))
+    win.button_7.clicked.connect(lambda: add_input_to_text(7, InputType.BUTTON))
+    win.button_8.clicked.connect(lambda: add_input_to_text(8, InputType.BUTTON))
+    win.button_9.clicked.connect(lambda: add_input_to_text(9, InputType.BUTTON))
+    win.button_decimal.clicked.connect(lambda: add_input_to_text(".", InputType.BUTTON))
 
-    win.button_multiply.clicked.connect(lambda: add_input_to_text('*'))
-    win.button_divide.clicked.connect(lambda: add_input_to_text('/'))
-    win.button_subtract.clicked.connect(lambda: add_input_to_text('-'))
-    win.button_add.clicked.connect(lambda: add_input_to_text('+'))
+    win.button_multiply.clicked.connect(lambda: add_input_to_text('*', InputType.BUTTON))
+    win.button_divide.clicked.connect(lambda: add_input_to_text('/', InputType.BUTTON))
+    win.button_subtract.clicked.connect(lambda: add_input_to_text('-', InputType.BUTTON))
+    win.button_add.clicked.connect(lambda: add_input_to_text('+', InputType.BUTTON))
 
-    win.button_clear.clicked.connect(lambda: clear(CLEAR))
-    win.button_backspace.clicked.connect(lambda: backspace(BACKSPACE))
-    win.button_evaluate.clicked.connect(lambda: evaluate(EVALUATE))
+    win.button_clear.clicked.connect(lambda: clear(CLEAR, InputType.BUTTON))
+    win.button_backspace.clicked.connect(lambda: backspace(BACKSPACE, InputType.BUTTON))
+    win.button_evaluate.clicked.connect(lambda: evaluate(EVALUATE, InputType.BUTTON))
 
 
 def key_press_event(event):
+    """
+    checks for key presses and reacts accordingly
+    """
+
     if Qt.QKeySequence(event.key()).toString() in ALLOWED_KEYS:
-        add_input_to_text(Qt.QKeySequence(event.key()).toString())
+        add_input_to_text(Qt.QKeySequence(event.key()).toString(), InputType.KEYBOARD)
 
     # Key_Return = "Normal" Enter, Key_Enter = NumPad Enter
     elif event.key() == Qt.Qt.Key_Return or event.key() == Qt.Qt.Key_Enter:
-        evaluate(EVALUATE)
+        evaluate(EVALUATE, InputType.KEYBOARD)
     elif event.key() == Qt.Qt.Key_Backspace:
-        backspace(BACKSPACE)
+        backspace(BACKSPACE, InputType.KEYBOARD)
 
 
 def override_key_press_event():
@@ -83,6 +106,9 @@ def override_key_press_event():
 
 
 def add_background_color():
+    """
+    Adds some background color to the buttons
+    """
     win.button_0.setStyleSheet("background-color: lightblue")
     win.button_1.setStyleSheet("background-color: lightblue")
     win.button_2.setStyleSheet("background-color: lightblue")
